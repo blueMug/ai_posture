64e83efd4940eb85103117ca6be5488703450578
huangyan <huangyan.3@bytedance.com>
2026-06-20 13:45:30 +0800

    Curate unique recommended pose categories

--- pages/home/index.js
+++ pages/home/index.js
@@ -2,14 +2,54 @@ const { poseTemplates, poseCategories } = require('../../utils/poses')
 const { cachePoseCategories } = require('../../utils/imageCache')
 
 const RECOMMEND_LIMIT_PER_CATEGORY = 4
+const RECOMMEND_CATEGORY_CONFIGS = [
+  {
+    sourceId: 'portrait-half',
+    name: '半身照',
+    subtitle: '精选近景半身、肩颈线条和温柔写真',
+    poseIds: [
+      'pair-custom1-r01-g01',
+      'pair-custom3-r01-g01',
+      'pair-custom19-r01-g01',
+      'pair-custom22-r01-g01'
+    ]
+  },
+  {
+    sourceId: 'selfie',
+    name: '自拍照',
+    subtitle: '适合手机自拍、窗边自然光和亲近感构图',
+    poseIds: [
+      'pair-custom18-r01-g01',
+      'pair-custom20-r01-g01',
+      'pair-custom21-r01-g01',
+      'pair-custom2-r01-g01'
+    ]
+  },
+  {
+    sourceId: 'outfit-standing',
+    name: '全身照',
+    subtitle: '精选显比例、穿搭展示和自然站姿',
+    poseIds: [
+      'pair-custom4-r01-g01',
+      'pair-custom6-r01-g01',
+      'pair-custom8-r01-g01',
+      'pair-custom9-r01-g01'
+    ]
+  },
+  {
+    sourceId: 'travel-back',
+    name: '背影照',
+    subtitle: '适合旅行、山海、湖畔和回眸背影',
+    poseIds: [
+      'pair-custom10-r01-g01',
+      'pair-custom13-r01-g01',
+      'pair-custom14-r01-g01',
+      'pair-custom17-r01-g01'
+    ]
+  }
+]
 
 const normalizeKeyword = (value) => String(value || '').trim().toLowerCase()
-const takeRecommendPoses = (categories) => categories
-  .map((category) => ({
-    ...category,
-    poses: category.poses.slice(0, RECOMMEND_LIMIT_PER_CATEGORY)
-  }))
-  .filter((category) => category.poses.length > 0)
 
 const getPoseSearchText = (pose, category) => [
   pose.name,
@@ -21,21 +61,52 @@ const getPoseSearchText = (pose, category) => [
   category.subtitle
 ].join(' ').toLowerCase()
 
+const poseTemplateMap = poseTemplates.reduce((map, pose) => {
+  map.set(pose.id, pose)
+  return map
+}, new Map())
+
+const poseCategoryMap = poseCategories.reduce((map, category) => {
+  map.set(category.id, category)
+  return map
+}, new Map())
+
+const buildRecommendCategories = (keyword = '') => {
+  const query = normalizeKeyword(keyword)
+  const usedPoseIds = new Set()
+
+  return RECOMMEND_CATEGORY_CONFIGS
+    .map((config) => {
+      const sourceCategory = poseCategoryMap.get(config.sourceId) || {}
+      const category = {
+        ...sourceCategory,
+        id: config.sourceId,
+        name: config.name,
+        subtitle: config.subtitle
+      }
+      const poses = config.poseIds
+        .map((poseId) => poseTemplateMap.get(poseId))
+        .filter(Boolean)
+        .filter((pose) => !usedPoseIds.has(pose.id))
+        .filter((pose) => !query || getPoseSearchText(pose, category).includes(query))
+        .slice(0, RECOMMEND_LIMIT_PER_CATEGORY)
+
+      poses.forEach((pose) => {
+        usedPoseIds.add(pose.id)
+      })
+
+      return {
+        ...category,
+        poses
+      }
+    })
+    .filter((category) => category.poses.length > 0)
+}
+
 const isRealPhotoPose = (pose) => Boolean(pose && pose.modelImage && pose.detailImage)
 
 const filterPoseCategories = (keyword) => {
-  const query = normalizeKeyword(keyword)
-
-  if (!query) {
-    return takeRecommendPoses(poseCategories)
-  }
-
-  return takeRecommendPoses(poseCategories
-    .map((category) => ({
-      ...category,
-      poses: category.poses.filter((pose) => getPoseSearchText(pose, category).includes(query))
-    }))
-    .filter((category) => category.poses.length > 0))
+  return buildRecommendCategories(keyword)
 }
 
 Page({
@@ -47,7 +118,7 @@ Page({
   },
 
   onLoad() {
-    this.setPoseCategories(takeRecommendPoses(poseCategories), {
+    this.setPoseCategories(buildRecommendCategories(), {
       hasSearchResult: true
     })
   },
@@ -87,7 +158,7 @@ Page({
   },
 
   clearSearch() {
-    this.setPoseCategories(takeRecommendPoses(poseCategories), {
+    this.setPoseCategories(buildRecommendCategories(), {
       searchKeyword: '',
       hasSearchResult: true
     })
