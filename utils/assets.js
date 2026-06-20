0012fae256aa9c48f875e1e77983d2b9c515f069
huangyan <huangyan.3@bytedance.com>
2026-06-20 14:03:06 +0800

    Use lightweight local camera guides

--- utils/assets.js
+++ utils/assets.js
@@ -1,13 +1,18 @@
 const USE_JSDELIVR_ASSETS = true
 const JSDELIVR_ASSET_BASE = 'https://cdn.jsdelivr.net/gh/blueMug/ai_posture@main'
-const LOCAL_PACKED_PREFIXES = ['/static/pose_pairs/', '/static/pose_thumbs/']
+const LOCAL_PACKED_PREFIXES = ['/static/pose_guides/', '/static/pose_thumbs/']
 
 const isRemoteUrl = (path) => /^https?:\/\//.test(path)
 const isPackedLocalAsset = (path) => LOCAL_PACKED_PREFIXES.some((prefix) => path.startsWith(prefix))
+const isPoseContour = (path) => path.startsWith('/static/pose_pairs/') && /_contour\.png$/.test(path)
 
 const assetUrl = (path) => {
   if (!path || isRemoteUrl(path)) {
     return path
+  }
+
+  if (isPoseContour(path)) {
+    return path.replace('/static/pose_pairs/', '/static/pose_guides/')
   }
 
   if (isPackedLocalAsset(path)) {
